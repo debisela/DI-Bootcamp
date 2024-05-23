eb0b1e5c2714e3c4b17afa47a66b4b8cfbd0f073
debisela <debora.jaeckel@gmail.com>
2024-05-24 00:30:59 +0300

    Day2 XP exercise 5

--- Week4/Day2/ExercisesXP/script.js
+++ Week4/Day2/ExercisesXP/script.js
@@ -203,6 +203,65 @@ Examples
 changeEnough(14.11, [2,100,0,0]) => returns false
 changeEnough(0.75, [0,0,20,5]) => returns true
 */
+function changeEnough(itemPrice, amountOfChange) {
+    let changeValues = [0.25, 0.1, 0.05, 0.01];
+    let changeSum = 0;
+    for (let i = 0; i < amountOfChange.length; i++){
+        changeSum += (amountOfChange[i] * changeValues[i]);    
+    } 
+    if (itemPrice <= changeSum) {
+        console.log(true)   
+    } 
+    else {
+        console.log(false)
+    }
+    }
+    changeEnough(4.25, [25,20,5,0]);
+    changeEnough(14.11, [2,100,0,0]);
+    changeEnough(0.75, [0,0,20,5]);
+
+    /*
+    Exercise 6 : Vacations Costs
+Instructions
+Let’s create functions that calculate your vacation’s costs:
+
+Define a function called hotelCost().
+It should ask the user for the number of nights they would like to stay in the hotel.
+If the user doesn’t answer or if the answer is not a number, ask again.
+The hotel costs $140 per night. The function should return the total price of the hotel.
+
+Define a function called planeRideCost().
+It should ask the user for their destination.
+If the user doesn’t answer or if the answer is not a string, ask again.
+The function should return a different price depending on the location.
+“London”: 183$
+“Paris” : 220$
+All other destination : 300$
+
+Define a function called rentalCarCost().
+It should ask the user for the number of days they would like to rent the car.
+If the user doesn’t answer or if the answer is not a number, ask again.
+Calculate the cost to rent the car. The car costs $40 everyday.
+If the user rents a car for more than 10 days, they get a 5% discount.
+The function should return the total price of the car rental.
+
+Define a function called totalVacationCost() that returns the total cost of the user’s vacation by calling the 3 functions that you created above.
+Example : The car cost: $x, the hotel cost: $y, the plane tickets cost: $z.
+Hint: You have to call the functions hotelCost(), planeRideCost() and rentalCarCost() inside the function totalVacationCost().
+
+Call the function totalVacationCost()
+
+Bonus: Instead of using a prompt inside the 3 first functions, only use a prompt inside the totalVacationCost() function. You need to change the 3 first functions, accordingly.
+*/
+
+
+
+
+
+
+
+
+
 
 
 
