6ed5afd060683a4b00779ea2583836fe9b037bc1
debisela <debora.jaeckel@gmail.com>
2024-05-23 23:36:12 +0300

    Day2 XP exercises 3-4

--- Week4/Day2/ExercisesXP/script.js
+++ Week4/Day2/ExercisesXP/script.js
@@ -73,8 +73,20 @@ At the end, console.log the sum of all numbers that are divisible by 23.
 Outcome : 0 23 46 69 92 115 138 161 184 207 230 253 276 299 322 345 368
 391 414 437 460 483
 Sum : 5313
+*/
+function isDivisible(){
+    let sum = 0;
+    for (let i = 0; i <= 500; i++) {
+        if (i%23==0) {
+            console.log(i);
+            sum += i;
+        }
+    }
+    console.log(sum);
+}
+isDivisible();
 
-
+/*
 Bonus: Add a parameter divisor to the function.
 
 isDivisible(divisor)
@@ -83,4 +95,124 @@ Example:
 isDivisible(3) : Console.log all the numbers divisible by 3, and their sum
 isDivisible(45) : Console.log all the numbers divisible by 45, and their sum
 */
+function isDivisible(divisor){
+    let sum = 0;
+    for (let i = 0; i <= 500; i++) {
+        if (i%divisor==0) {
+            console.log(i);
+            sum += i;
+        }
+    }
+    console.log(sum);
+}
+isDivisible(3);
+
+/*
+Exercise 4 : Shopping List
+Instructions
+const stock = { 
+    "banana": 6, 
+    "apple": 0,
+    "pear": 12,
+    "orange": 32,
+    "blueberry":1
+}  
+
+const prices = {    
+    "banana": 4, 
+    "apple": 2, 
+    "pear": 1,
+    "orange": 1.5,
+    "blueberry":10
+} 
+Add the stock and prices objects to your js file.
+
+Create an array called shoppingList with the following items: “banana”, “orange”, and “apple”. It means that you have 1 banana, 1 orange and 1 apple in your cart.
+
+Create a function called myBill() that takes no parameters.
+
+The function should return the total price of your shoppingList. In order to do this you must follow these rules:
+The item must be in stock. (Hint : check out if .. in)
+If the item is in stock find out the price in the prices object.
+
+Call the myBill() function.
+
+Bonus: If the item is in stock, decrease the item’s stock by 1
+*/
+const stock = { 
+    "banana": 6, 
+    "apple": 0,
+    "pear": 12,
+    "orange": 32,
+    "blueberry":1
+}  
+
+const prices = {    
+    "banana": 4, 
+    "apple": 2, 
+    "pear": 1,
+    "orange": 1.5,
+    "blueberry":10
+}
+let shoppingList = ["banana", "orange", "apple"]
+function myBill(){
+    let totalPrice = 0;
+    for (let x of shoppingList) {
+        if (x in stock && stock[x] > 0){
+            totalPrice += prices[x];
+            stock[x]--;
+        }
+        
+    }  
+    console.log("the price is " + totalPrice)
+    console.log(stock)
+}
+myBill()
+/*
+Exercise 5 : What’s In My Wallet ?
+Instructions
+Note: Read the illustration (point 4), while reading the instructions
+
+Create a function named changeEnough(itemPrice, amountOfChange) that receives two arguments :
+an item price
+and an array representing the amount of change in your pocket.
+
+In the function, determine whether or not you can afford the item.
+If the sum of the change is bigger or equal than the item’s price (ie. it means that you can afford the item), the function should return true
+If the sum of the change is smaller than the item’s price (ie. it means that you cannot afford the item) the function should return false
+
+Change will always be represented in the following order: quarters, dimes, nickels, pennies.
+A quarters is 0.25
+A dimes is 0.10
+A nickel is 0.05
+A penny is 0.01
+
+
+4. To illustrate:
+
+After you created the function, invoke it like this:
+
+changeEnough(4.25, [25, 20, 5, 0])
+The value 4.25 represents the item’s price
+The array [25, 20, 5, 0] represents 25 quarters, 20 dimes, 5 nickels and 0 pennies.
+The function should return true, since having 25 quarters, 20 dimes, 5 nickels and 0 pennies gives you 6.25 + 2 + .25 + 0 = 8.50 which is bigger than 4.25 (the total amount due)
+
+
+Examples
+
+changeEnough(14.11, [2,100,0,0]) => returns false
+changeEnough(0.75, [0,0,20,5]) => returns true
+*/
+
+
+
+
+
+
+
+
+
+
+
+
 
