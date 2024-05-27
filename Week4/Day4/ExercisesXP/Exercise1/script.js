c0510556b7f4eaa24a1e68525773a61fc8d7b63a
debisela <debora.jaeckel@gmail.com>
2024-05-27 20:21:11 +0300

    week4, day4, XP, exercise 1

--- Week4/Day4/ExercisesXP/Exercise1/script.js
+++ Week4/Day4/ExercisesXP/Exercise1/script.js
@@ -29,13 +29,22 @@ console.log(Pete.textContent);
 /*
 Delete the <li> that contains the text node “Sarah”. (It’s the second <li> of the second <ul>)
 */
-let Sarah = document.getElementsByClassName('list')[1].children[1];
-console.log(document.removeChild(Sarah));
 
+let list2 = document.getElementsByClassName('list')[1];
+let Sarah = document.getElementsByClassName('list')[1].children[1];
+list2.removeChild(Sarah);
+console.log(list2);
 
 /*
 Change each first name of the two <ul>'s to your name. (Hint : use a loop)
+*/
 
+let John = document.getElementsByClassName('list')[0].children[0];
+John.textContent = "Debi";
+let David = document.getElementsByClassName('list')[1].children[0];
+David.textContent = "Debi";
+
+/*
 Bonus - Using Javascript:
 Add a class called student_list to both of the <ul>'s.
 Add the classes university and attendance to the first <ul>.
