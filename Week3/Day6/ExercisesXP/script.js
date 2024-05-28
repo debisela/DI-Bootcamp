02ce62cef9b8de4802b599d51bc04cf7d3ad68e0
debisela <debora.jaeckel@gmail.com>
2024-05-28 11:52:48 +0300

    week3,day6,XP exercise 4

--- Week3/Day6/ExercisesXP/script.js
+++ Week3/Day6/ExercisesXP/script.js
@@ -88,7 +88,7 @@ While the number is smaller than 10 continue asking the user for a new number.
 Tip : Which while loop is more relevant for this situation?
 */
 
-let numberPrompt;
+/*let numberPrompt;
 numberPrompt= prompt("Please insert a number");
 numberPrompt = Number(numberPrompt);
 if (typeof numberPrompt == 'number') {
@@ -103,6 +103,88 @@ else {
     alert("this is not a number");
 }
 
+/*
+🌟 Exercise 4 : Building Management
+Instructions:
+const building = {
+    numberOfFloors: 4,
+    numberOfAptByFloor: {
+        firstFloor: 3,
+        secondFloor: 4,
+        thirdFloor: 9,
+        fourthFloor: 2,
+    },
+    nameOfTenants: ["Sarah", "Dan", "David"],
+    numberOfRoomsAndRent:  {
+        sarah: [3, 990],
+        dan:  [4, 1000],
+        david: [1, 500],
+    },
+}
 
+
+Review About Objects
+Copy and paste the above object to your Javascript file.
+*/
+const building = {
+    numberOfFloors: 4,
+    numberOfAptByFloor: {
+        firstFloor: 3,
+        secondFloor: 4,
+        thirdFloor: 9,
+        fourthFloor: 2,
+    },
+    nameOfTenants: ["Sarah", "Dan", "David"],
+    numberOfRoomsAndRent:  {
+        sarah: [3, 990],
+        dan:  [4, 1000],
+        david: [1, 500],
+    },
+}
+
+/*
+
+Console.log the number of floors in the building.
+
+Console.log how many apartments are on the floors 1 and 3.
+
+Console.log the name of the second tenant and the number of rooms he has in his apartment.
+
+Check if the sum of Sarah’s and David’s rent is bigger than Dan’s rent. If it is, than increase Dan’s rent to 1200.*/
+
+console.log(building.numberOfFloors);
+console.log("in floor 1 there are " + building.numberOfAptByFloor.firstFloor + " and in floor 3 there are " + building.numberOfAptByFloor.thirdFloor + " apartments");
+
+
+
+
+
+
+
+/*
+🌟 Exercise 5 : Family
+Instructions
+Create an object called family with a few key value pairs.
+Using a for in loop, console.log the keys of the object.
+Using a for in loop, console.log the values of the object.
+
+
+Exercise 6 : Rudolf
+Instructions
+const details = {
+  my: 'name',
+  is: 'Rudolf',
+  the: 'reindeer'
+}
+Given the object above and using a for loop, console.log “my name is Rudolf the reindeer”
+
+
+Exercise 7 : Secret Group
+Instructions
+const names = ["Jack", "Philip", "Sarah", "Amanda", "Bernard", "Kyle"];
+A group of friends have decided to start a secret society. The society’s name will be the first letter of each of their names sorted in alphabetical order.
+Hint: a string is an array of letters
+Console.log the name of their secret society. The output should be “ABJKPS”
+*/
 
 
