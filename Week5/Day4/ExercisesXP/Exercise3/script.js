8ef8c0976eb488e5d9e94ba3b17b1e7074aae90a
debisela <debora.jaeckel@gmail.com>
2024-06-02 21:54:46 +0300

    week5,day4,XP,Ex3

--- Week5/Day4/ExercisesXP/Exercise3/script.js
+++ Week5/Day4/ExercisesXP/Exercise3/script.js
@@ -3,24 +3,25 @@ Copy the code above, to a structured HTML file.
 In your Javascript file add the functionality which will allow you to drag the box and drop it into the target. Check out the Course Notes named DOM animations
 */
 
+
 let smallBox = document.getElementById('box');
 let bigBox = document.getElementById('target');
 
-smallBox.addEventListener('dragstart', dragStart)
 
-function dragStart(event){
-    event.dataTransfer.setData('text', event.target.id);
-}
-bigBox.addEventListener('dragover', dragOver)
-bigBox.addEventListener('drop', drop)
-bigBox.addEventListener('dragenter', dragEnter)
-bigBox.addEventListener('dragleave', dragLeave)
 
-function dragOver(event){
-    event.prevenDefault();
+function onDragStart(event) {
+    event.dataTransfer.setData("text/plain", event.target.id);
+    event.dataTransfer.effectAllowed = "move";
 }
-function drop(event){
-    event.prevenDefault();
-    let smallBoxdata = event.dataTransfer.getData("text");
+
+function onDragover(event) {
+    event.preventDefault(); 
+    event.dataTransfer.dropEffect = "move";
+}
+
+function onDrop(event) {
+    event.preventDefault(); 
+    var smallBoxdata = event.dataTransfer.getData("text/plain");
     
+    event.target.appendChild(document.getElementById(smallBoxdata));
 }
