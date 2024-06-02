c721f1f82f099b81cd7dcf21eac49c2e9d5a3096
debisela <debora.jaeckel@gmail.com>
2024-06-02 15:43:47 +0300

    week5,day4,XP,Ex1,in progress

---
+++ Week5/Day4/ExercisesXP/Exercise1/script.js
@@ -0,0 +1,67 @@
+/*
+🌟 Exercise 1: Timer
+Instructions
+Using this HTML code:
+
+<!DOCTYPE html>
+    <html>
+    <head>
+        <style>
+        p {
+          background: yellow;
+          color: red;
+        }
+        </style>
+    </head>
+    <body>
+        <div id="container"></div>
+        <button id="clear">Clear Interval</button>
+    </body>
+    </html>
+
+
+Part I
+In your Javascript file, using setTimeout, call a function after 2 seconds.
+The function will alert “Hello World”.
+*/
+/*function sayHello(){
+    alert("Hello, World")
+}
+setTimeout(sayHello, 2000);
+/*
+Part II
+In your Javascript file, using setTimeout, call a function after 2 seconds.
+The function will add a new paragraph <p>Hello World</p> to the <div id="container">.*/
+
+/*function newPar(){
+    let cont = document.getElementById('container');
+    let newP = document.createElement('p');
+    let newPContent = document.createTextNode("Hello World");
+    newP.appendChild(newPContent);
+    cont.appendChild(newP);
+}
+setTimeout(newPar, 2000);
+
+/*
+Part III
+In your Javascript file, using setInterval, call a function every 2 seconds.
+The function will add a new paragraph <p>Hello World</p> to the <div id="container">.
+The interval will be cleared (ie. clearInterval), when the user will click on the button.
+Instead of clicking on the button, the interval will be cleared (ie. clearInterval) as soon as there will be 5 paragraphs inside the <div id="container">.*/
+
+function newPar(){
+    let cont = document.getElementById('container');
+    let newP = document.createElement('p');
+    let newPContent = document.createTextNode("Hello World");
+    newP.appendChild(newPContent);
+    cont.appendChild(newP);
+}
+
+const myInterval = setInterval(newPar, 2000);
+function stopInterval(){
+    clearInterval(myInterval);
+}
+
+document.getElementById('clear').addEventListener('click', stopInterval);
+
+
