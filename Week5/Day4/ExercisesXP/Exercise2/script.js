0793265928b1c626c14877778b17c7315a276573
debisela <debora.jaeckel@gmail.com>
2024-06-03 22:04:34 +0300

    week5,day4,XP,Ex2

--- Week5/Day4/ExercisesXP/Exercise2/script.js
+++ Week5/Day4/ExercisesXP/Exercise2/script.js
@@ -7,13 +7,12 @@ Hint : check out the demonstration in the Course Noted named JS Function*/
 function myMove() {
     var elem = document.getElementById("animate");   
     var pos = 0;
-    var id = setInterval(frame, 5);
+    var id = setInterval(frame, 1);
     function frame() {
       if (pos == 350) {
         clearInterval(id);
       } else {
-        pos++; 
-        //elem.style.top = pos + 'px'; 
+        pos++;  
         elem.style.left = pos + 'px'; 
       }
     }
