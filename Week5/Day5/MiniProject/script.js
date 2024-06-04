13e815810ab801b747835c9bedc18b3d4db737ae
debisela <debora.jaeckel@gmail.com>
2024-06-04 15:30:11 +0300

    week5,day5,mini project, prepare layout

--- Week5/Day5/MiniProject/script.js
+++ Week5/Day5/MiniProject/script.js
@@ -1,9 +1,9 @@
 
 for (let i = 0; i <= 21; i++) {
     let newColorDiv = document.createElement('div');
-    newColorDiv.className = "color-fields"+i;
-    let randomColor = Math.floor(Math.random()*16777215).toString(16);
-    newColorDiv.style.backgroundColor = 'blue'
+    newColorDiv.className = "color-fields";
+    //let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    newColorDiv.style.backgroundColor = 'blue';
     document.getElementById('sidebar').appendChild(newColorDiv)
     
 }
