ecc868970cebf81c195a7e2e75b28bbe40b2af7b
debisela <debora.jaeckel@gmail.com>
2024-06-04 23:33:55 +0300

    week5,day5,mini project, choose color, color grid-field

--- Week5/Day5/MiniProject/script.js
+++ Week5/Day5/MiniProject/script.js
@@ -2,11 +2,11 @@
 for (let i = 0; i < 21; i++) {
     let newColorDiv = document.createElement('div');
     newColorDiv.className = "color-fields";
-  
     let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
     newColorDiv.style.backgroundColor = randomColor;
-    document.getElementById('color-pallette').appendChild(newColorDiv)
-    
+    document.querySelector('.pallette').appendChild(newColorDiv)
+    console.log(randomColor);
+    console.log(newColorDiv.style.backgroundColor)
 }
 
 for (let i = 0; i <= 1791; i++) {
@@ -15,3 +15,27 @@ for (let i = 0; i <= 1791; i++) {
     document.getElementById('main').appendChild(newDiv)
     
 }
+let colorFields = document.querySelectorAll('.color-fields');
+
+colorFields.forEach(function(elem) {
+    elem.addEventListener('click', chooseColor);
+    function chooseColor(){
+        let colorName = elem.style.backgroundColor
+        console.log(colorName);
+        return elem.style.backgroundColor;
+    }
+});
+
+let drawingGrid = document.getElementById('main');
+let drawingFields = document.querySelectorAll('.drawing-fields');
+drawingFields.forEach(function(elem) {
+    elem.addEventListener('mousedown', draw);
+    function draw(){
+        elem.style.backgroundColor = 'red'
+    }
+});
+
+
+
+
+
