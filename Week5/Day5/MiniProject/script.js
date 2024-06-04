9c1727a16f4d26616b2c1cdaf85e55925945eb6b
debisela <debora.jaeckel@gmail.com>
2024-06-04 23:52:53 +0300

    week5,day5,mini project, choose color, color grid-field

--- Week5/Day5/MiniProject/script.js
+++ Week5/Day5/MiniProject/script.js
@@ -1,8 +1,18 @@
+//took array with 21 random colors
+
+let colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+'#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+'#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+'#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+'#66994D']
+
 
 for (let i = 0; i < 21; i++) {
     let newColorDiv = document.createElement('div');
     newColorDiv.className = "color-fields";
-    let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    //tried first a version with random number generation but did not manage to save the color as variable
+    /*let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);*/
+    let randomColor = colors[i];
     newColorDiv.style.backgroundColor = randomColor;
     document.querySelector('.pallette').appendChild(newColorDiv)
     console.log(randomColor);
@@ -16,7 +26,6 @@ for (let i = 0; i <= 1791; i++) {
     
 }
 let colorFields = document.querySelectorAll('.color-fields');
-
 colorFields.forEach(function(elem) {
     elem.addEventListener('click', chooseColor);
     function chooseColor(){
@@ -31,7 +40,7 @@ let drawingFields = document.querySelectorAll('.drawing-fields');
 drawingFields.forEach(function(elem) {
     elem.addEventListener('mousedown', draw);
     function draw(){
-        elem.style.backgroundColor = 'red'
+        elem.style.backgroundColor = 'black'
     }
 });
 
