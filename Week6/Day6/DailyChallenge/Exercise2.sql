406b0df7a89ecfa68c949c3d244a72d21c046f83
debisela <debora.jaeckel@gmail.com>
2024-06-15 22:57:07 +0300

    Week6,Day6,Daily

---
+++ Week6/Day6/DailyChallenge/Exercise2.sql
@@ -0,0 +1,3 @@
+insert into actors (first_name, last_name, age, number_oscars)
+values('Ryan','Reynolds','','');
+-- I expect that the record will not be added since we defined NOT NULL for all fields
