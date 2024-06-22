7f721ced9fdf51488fb35d37b652d35baf8c6ff4
debisela <debora.jaeckel@gmail.com>
2024-06-22 21:30:14 +0300

    Week7,Day6,Ex5

--- Week7/Day6/ExercisesXP/Ex4_5/script.js
+++ Week7/Day6/ExercisesXP/Ex4_5/script.js
@@ -19,6 +19,9 @@ console.log(`${i+1}# choice is ${colors[i]}`);
         console.log("No...")
     }*/
 
+//with ternary operator:
+const colorsInclude = colors.includes("Violet")?console.log("Yeah"):console.log("No...")
+
 
 
 
@@ -33,3 +36,25 @@ const ordinal = ["th","st","nd","rd"];
 Write a JavaScript program that displays the colors in the following order : “1st choice is Blue .” “2nd choice is Green.” “3rd choice is Red.” ect…
 Hint : Use the array methods taught in class and ternary operator.*/
 const ordinal = ["th","st","nd","rd"];
+
+/*for (let i = 0; i < colors.length; i++){
+    if (i+1 === 1){
+        console.log(`${i+1} ${ordinal[1]} choice is ${colors[i]}`)
+}
+else if (i+1 === 2){
+    console.log(`${i+1} ${ordinal[2]} choice is ${colors[i]}`)
+    }
+    else if (i+1 === 3){
+        console.log(`${i+1} ${ordinal[3]} choice is ${colors[i]}`)
+        }
+    else {
+        console.log(`${i+1} ${ordinal[0]} choice is ${colors[i]}`)
+    }
+}*/
+//with ternary operator:
+for (let i = 0; i < colors.length; i++){
+    const suffix = i+1 === 1?console.log(`${i+1} ${ordinal[1]} choice is ${colors[i]}`):
+    i+1 === 2?console.log(`${i+1} ${ordinal[2]} choice is ${colors[i]}`):
+    i+1 === 3?console.log(`${i+1} ${ordinal[3]} choice is ${colors[i]}`):
+    console.log(`${i+1} ${ordinal[0]} choice is ${colors[i]}`)
+    }
