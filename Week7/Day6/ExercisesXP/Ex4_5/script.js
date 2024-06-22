f8ed12d94e6972d143b1e39bd4140aedbbe3084c
debisela <debora.jaeckel@gmail.com>
2024-06-22 13:42:46 +0300

    Week7,Day6,Ex3

---
+++ Week7/Day6/ExercisesXP/Ex4_5/script.js
@@ -0,0 +1,22 @@
+/*
+🌟 Exercise 4 : Colors
+Instructions
+Using this array :
+
+const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+Write a JavaScript program that displays the colors in the following order : “1# choice is Blue.” “2# choice is Green.” “3# choice is Red.” ect…
+Check if at least one element of the array is equal to the value “Violet”. If yes, console.log("Yeah"), else console.log("No...")
+Hint : Use the array methods taught in class. Look at the lesson Array Methods.*/
+const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+
+
+
+/*
+🌟 Exercise 5 : Colors #2
+Instructions
+Using these arrays :
+
+const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+const ordinal = ["th","st","nd","rd"];
+Write a JavaScript program that displays the colors in the following order : “1st choice is Blue .” “2nd choice is Green.” “3rd choice is Red.” ect…
+Hint : Use the array methods taught in class and ternary operator.*/
