1be4367d9fa6bcc075067a04fcb3891e8224009f
debisela <debora.jaeckel@gmail.com>
2024-06-24 21:30:39 +0300

    Week8,Day2,XP,Ex4-5

--- Week8/Day2/ExercisesXP/script.js
+++ Week8/Day2/ExercisesXP/script.js
@@ -60,4 +60,47 @@ Create a Bootstrap Navbar in your HTML file.
 In your js file, create a self invoking funtion that takes 1 argument: the name of the user that just signed in.
 The function should add a div in the nabvar, displaying the name of the user and his profile picture.*/
 
+/*
+🌟 Exercise 5 : Juice Bar
+Instructions
+You will use nested functions, to open a new juice bar.
+
+Part I:
+The outer function named makeJuice receives 1 argument: the size of the beverage the client wants - small, medium or large.
+
+The inner function named addIngredients receives 3 ingredients, and displays on the DOM a sentence like The client wants a <size drink> juice, containing <first ingredient>, <second ingredient>, <third ingredient>".
+
+Invoke the inner function ONCE inside the outer function. Then invoke the outer function in the global scope.*/
+
+function makeJuice(size) {
+    let ingredients = [];
+function addIngredients(ingredient1,ingredient2,ingredient3){
+    let newDiv = document.createElement('div');
+let divText = document.createTextNode("The client wants a "+size+" size juice, containing "+ingredient1+", "+ingredient2+", "+ingredient3+".");
+newDiv.appendChild(divText);
+    document.body.appendChild(newDiv)
+    ingredients.push(ingredient1,ingredient2,ingredient3)
+    console.log(ingredients);
+}
+function displayJuice(){
+    let newDiv = document.createElement('div');
+let divText = document.createTextNode("The client wants a "+size+" size juice, containing "+(ingredients));
+newDiv.appendChild(divText);
+    document.body.appendChild(newDiv)
+}
+addIngredients("orange","apple","mango");
+addIngredients("banana","strawberry","lemon")
+displayJuice()
+}
+makeJuice("medium");
+
+/*
+Part II:
+In the makeJuice function, create an empty array named ingredients.
+
+The addIngredients function should now receive 3 ingredients, and push them into the ingredients array.
+
+Create a new inner function named displayJuice that displays on the DOM a sentence like The client wants a <size drink> juice, containing <first ingredient>, <second ingredient>, <third ingredient>".
+
+The client wants 6 ingredients in his juice, therefore, invoke the addIngredients function TWICE. Then invoke once the displayJuice function. Finally, invoke the makeJuice function in the global scope.*/
 
