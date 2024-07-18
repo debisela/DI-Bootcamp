17634c1b4846c4298b7d8a9eb456e196d0d6a208
debisela <debora.jaeckel@gmail.com>
2024-07-18 12:40:10 +0300

    Week11,day2,Ex1

--- Week11/Day2/ExerciseXP/Ex1/shop.js
+++ Week11/Day2/ExerciseXP/Ex1/shop.js
@@ -1,7 +1,17 @@
 const products = require('./products.js')
 console.log(products);
 
-// const searchProduct = (name) =>{
-// console.log(obj.products.name);
-// }
-// searchProduct('Laptop');
+function searchProduct(productName) {
+const product = products.find(p => p.name.toLocaleLowerCase() === productName.toLocaleLowerCase());
+if (product){
+    console.log(`product found, ${product.name}, ${product.price}, ${product.category}`);
+}
+else{
+    console.log(`Sorry, product not found`);
+}
+}
+
+//test
+searchProduct('blender');
+searchProduct('smartphone');
+searchProduct('phone')
