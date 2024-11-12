29ccf7886b8f06f5527b26e75a94762dd581e8d0
debisela <debora.jaeckel@gmail.com>
2024-11-12 15:34:54 +0200

    challenge2

--- Week25/Day6/DailyChallenge/daily.py
+++ Week25/Day6/DailyChallenge/daily.py
@@ -9,5 +9,32 @@ for index, letter in enumerate(user_word):
 print(letters_dict)
 
 #challenge2
+items_purchase = {
+   "Apple": "$4",
+  "Honey": "$3",
+  "Fan": "$14",
+  "Bananas": "$4",
+  "Pan": "$100",
+  "Spoon": "$2"
+}
+wallet = "$1"
+new_wallet=int(wallet[1:])
+selected_items=[]
+total_cost=0
+
+for product,price in items_purchase.items():
+    new_price=int(price[1:].replace(',',''))
+    if total_cost+new_price<=new_wallet:
+        selected_items.append(product)
+        total_cost+=new_price
+    if total_cost==new_wallet:
+        break
+
+if selected_items:
+    print(sorted(selected_items))
+else:
+    print("nothing")
+
+
 
    
