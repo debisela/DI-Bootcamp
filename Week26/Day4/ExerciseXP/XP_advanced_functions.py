04fd4c5dc486995ef5b5aadb159608da58c3c25a
debisela <debora.jaeckel@gmail.com>
2024-11-14 20:58:24 +0200

    xp1,2,4

---
+++ Week26/Day4/ExerciseXP/XP_advanced_functions.py
@@ -0,0 +1,98 @@
+#exercise1
+import random
+def get_random_temp(season):
+    if season=="winter":
+        random_number=random.randint(-10,16)
+    elif season=="autumn" or season=="spring":
+        random_number=random.randint(17,23)
+    else:
+        random_number=random.randint(24,40)
+    return random_number
+
+def main():
+    season_user=input("please enter the current season")
+    temp=get_random_temp(season_user)
+    print(f"The temperature right now is {temp} degrees Celsius")
+    if temp<0:
+        print("Brrr, that’s freezing! Wear some extra layers today")
+    elif temp>=0 and temp<=16:
+        print("Quite chilly! Don’t forget your coat")
+    elif temp>16 and temp<=23:
+        print("a sweather should be enough")
+    elif temp>23 and temp<=32:
+        print("it is hot outside, pack some sunscreen")
+    else:
+        print("it is very hot outside, take some water and stay in the shadow")
+
+main()
+
+#exercise2:
+data = [
+    {
+        "question": "What is Baby Yoda's real name?",
+        "answer": "Grogu"
+    },
+    {
+        "question": "Where did Obi-Wan take Luke after his birth?",
+        "answer": "Tatooine"
+    },
+    {
+        "question": "What year did the first Star Wars movie come out?",
+        "answer": "1977"
+    },
+    {
+        "question": "Who built C-3PO?",
+        "answer": "Anakin Skywalker"
+    },
+    {
+        "question": "Anakin Skywalker grew up to be who?",
+        "answer": "Darth Vader"
+    },
+    {
+        "question": "What species is Chewbacca?",
+        "answer": "Wookiee"
+    }
+]
+
+
+
+def ask_user(data):
+    correct_points=0
+    wrong_points=0
+    wrong_answers=[]
+    for question_answer in data:
+        user_answer=input(question_answer['question'])
+        if user_answer==question_answer['answer']:
+            correct_points+=1
+        else:
+            wrong_points+=1
+            wrong_answers.append(user_answer)
+    return (f"correct answers: {correct_points}, wrong answers: {wrong_points}, the wrong answer was: {wrong_answers}")
+
+result=ask_user(data)
+print(result)
+
+#exercise3:
+
+#exercise4:
+def get_parameter(a):
+    if isinstance(a, int):
+        result = 0
+        for i in range(1, 5):
+            result += int(str(a) * i)
+        return result
+    else:
+        print("Please enter a number")
+result=get_parameter(3)
+print(result)
+
+
+
+
+
+
+
+    
+    
+    
+
